7bfd734972c60117010ef87f566a4d71976d3bfb
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-22 23:00:36 +0200

    backend/feat/3/add_user_class: `User` class
    - [x] Add method borrow
        - User borrows a book.
    - [x] Add method is_borrowed
        - Check if the user borrowed the book or not.

--- user.py
+++ user.py
@@ -4,6 +4,15 @@ class User:
         self.id = id
         self.borrowed_books = []
 
+    def borrow(self, book):
+        self.borrowed_books.append(book)
+
+    def is_borrowed(self, book):
+        for mybook in self.borrowed_books:
+            if mybook.id == book.id:
+                return True
+        return False
+
     def __repr__(self):
         ret = f'User name: {self.name:15} - id: {self.id}\n\tBorrowed books:\n'
         for book in self.borrowed_books:

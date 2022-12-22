26bfa5ac0f1549d5621bb8a36978c86a2d2e10fa
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-23 01:39:11 +0200

    frontend/feat/8/implement_run_method

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from frontend import FrontendManager
 
 if __name__ == '__main__':
     app = FrontendManager()
-    app.print_menu()
+    app.run()
--- frontend.py
+++ frontend.py
@@ -23,3 +23,28 @@ class FrontendManager:
 
         msg = f'Enter your choice (from 1 to {len(messages)}): '
         return input_valid_int(msg, 1, len(messages))
+
+    def run(self):
+        while True:
+            choice = self.print_menu()
+            if choice == 1:
+                self.add_book()
+            # elif choice == 2:
+            #     self.print_books()
+            # elif choice == 3:
+            #     self.print_name_prefix()
+            # elif choice == 4:
+            #     self.add_user()
+            # elif choice == 5:
+            #     self.borrow_book()
+            # elif choice == 6:
+            #     self.return_book()
+            # elif choice == 7:
+            #     self.print_users_borrowed_book()
+            # elif choice == 8:
+            #     self.print_users()
+            else:
+                break
+
+    def add_book(self):
+        pass

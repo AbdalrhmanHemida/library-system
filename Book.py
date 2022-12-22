1345b152ba67b2290bd3e428fe7e4655a74bf0ca
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-22 21:26:57 +0200

    backend/feat/1/add_book_class: Borrow method

--- Book.py
+++ Book.py
@@ -5,6 +5,12 @@ class Book:
         self.total_quantity = total_quantity
         self.total_borrowed = 0
 
+    def borrow(self):
+        if self.total_quantity - self.total_borrowed == 0:
+            return False
+        self.total_borrowed += 1
+        return True
+
     def __repr__(self):
         return f'Book name: {self.name:20} - id: {self.id} - total quantity: {self.total_quantity} - ' \
                f'total borrowed: {self.total_borrowed}'

8d4e043271cae2002527448180ee0326f6ecfde0
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-22 21:30:35 +0200

    backend/feat/1/add_book_class: return_copy method

--- Book.py
+++ Book.py
@@ -11,6 +11,10 @@ class Book:
         self.total_borrowed += 1
         return True
 
+    def return_copy(self):
+        assert self.total_borrowed > 0
+        self.total_borrowed -= 1
+        
     def __repr__(self):
         return f'Book name: {self.name:20} - id: {self.id} - total quantity: {self.total_quantity} - ' \
                f'total borrowed: {self.total_borrowed}'

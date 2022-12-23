7651fe26d2957e34c67b41e610c289e272d17ef7
Abdalrhman Hamida <abdalrhman7mida@gmail.com>
2022-12-23 14:24:47 +0200

    backend/feat/10/backendmanger_class: `print_users` method

--- frontend.py
+++ frontend.py
@@ -34,16 +34,16 @@ class FrontendManager:
             #     self.print_books()
             # elif choice == 3:
             #     self.print_name_prefix()
-            # elif choice == 4:
-            #     self.add_user()
+            elif choice == 4:
+                self.add_user()
             # elif choice == 5:
             #     self.borrow_book()
             # elif choice == 6:
             #     self.return_book()
             # elif choice == 7:
             #     self.print_users_borrowed_book()
-            # elif choice == 8:
-            #     self.print_users()
+            elif choice == 8:
+                self.print_users()
             else:
                 break
 
@@ -59,3 +59,7 @@ class FrontendManager:
         id = input('Book id: ')
         total_quantity = int(input('Total quantity: '))
         self.backend.add_book(name, id, total_quantity)
+
+    def print_users(self):
+        users_str = '\n'.join([str(user) for user in self.backend.users])
+        print(users_str)

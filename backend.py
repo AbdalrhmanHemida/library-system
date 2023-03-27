2a730adbe4a9e7bd80b52178be52efccbdfaf5e8
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-03-28 01:44:23 +0200

    backend/refactor/14/type_annotations: Add type_annotation

--- backend.py
+++ backend.py
@@ -1,34 +1,35 @@
 from book import Book
 from user import User
+from typing import List, Optional
 
 
 class BackendManger:
-    def __init__(self):
+    def __init__(self) -> None:
         self.books = []
         self.users = []
 
-    def add_user(self, name, id):
+    def add_user(self, name: str, id: str) -> None:
         self.users.append(User(name, id))
 
-    def add_book(self, name, id, total_quantity):
+    def add_book(self, name: str, id: str, total_quantity: int) -> None:
         self.books.append(Book(name, id, total_quantity))
 
-    def get_books_with_prefix(self, prefix):
+    def get_books_with_prefix(self, prefix: str) -> List[Book]:
         return [book for book in self.books if book.name.startswith(prefix)]
 
-    def get_user_by_name(self, name):
+    def get_user_by_name(self, name: str) -> Optional[User]:
         for user in self.users:
             if name == user.name:
                 return user
         return None
 
-    def get_book_by_name(self, name):
+    def get_book_by_name(self, name: str) -> Optional[Book]:
         for book in self.books:
             if name == book.name:
                 return book
         return None
 
-    def borrow_book(self, user_name, book_name):
+    def borrow_book(self, user_name: str, book_name: str) -> bool:
         user = self.get_user_by_name(user_name)
         book = self.get_book_by_name(book_name)
 
@@ -40,7 +41,7 @@ class BackendManger:
             return True
         return False
 
-    def return_book(self, user_name, book_name):
+    def return_book(self, user_name: str, book_name: str) -> None:
         user = self.get_user_by_name(user_name)
         book = self.get_book_by_name(book_name)
 
@@ -53,7 +54,7 @@ class BackendManger:
         else:
             print('This user did not borrow this book')
 
-    def get_users_borrowed_book(self, book_name):
+    def get_users_borrowed_book(self, book_name: str) -> List[User]:
         book = self.get_book_by_name(book_name)
 
         if book is None:

f55e784c0386e03e761454b2a5def0f705238a80
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-03-28 01:30:09 +0200

    backend/refactor/14/type_annotations: Add type annotation

--- Book.py
+++ Book.py
@@ -1,20 +1,20 @@
 class Book:
-    def __init__(self, name, id, total_quantity):
+    def __init__(self, name: str, id: str, total_quantity: int) -> None:
         self.name = name
         self.id = id
         self.total_quantity = total_quantity
         self.total_borrowed = 0
 
-    def borrow(self):
+    def borrow(self) -> bool:
         if self.total_quantity - self.total_borrowed == 0:
             return False
         self.total_borrowed += 1
         return True
 
-    def return_copy(self):
+    def return_copy(self) -> None:
         assert self.total_borrowed > 0
         self.total_borrowed -= 1
-        
-    def __repr__(self):
+
+    def __repr__(self) -> str:
         return f'Book name: {self.name:20} - id: {self.id} - total quantity: {self.total_quantity} - ' \
                f'total borrowed: {self.total_borrowed}'
